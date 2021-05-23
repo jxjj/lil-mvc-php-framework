32a130389c9bb9ec4658179749e365e8b55644c8
James Johnson <980170+jxjj@users.noreply.github.com>
2021-05-22 22:34:30 -0500

    simple template rendering

---
+++ core/helpers.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!function_exists("array_reduce_assoc")) {
+  /**
+   * reduce for associative arrays
+   *
+   * @param array $array
+   * @param callable $callback ($carry, $key, $value) => $newcarry
+   * @param mixed $initial
+   */
+  function array_reduce_assoc(array $array, callable $callback, $initial = null)
+  {
+    $carry = $initial;
+    foreach ($array as $key => $value) {
+      $carry = $callback($carry, $key, $value);
+    }
+    return $carry;
+  }
+}
+
+if (!function_exists("dd")) {
+  function dd($var)
+  {
+    echo "<pre>";
+    var_dump(htmlspecialchars($var));
+    echo "</pre>";
+    die();
+  }
+}
