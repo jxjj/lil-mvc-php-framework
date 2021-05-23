3c6a52597a43ae0c48a21034ae58237276dfef58
James Johnson <johnsojr@gmail.com>
2021-05-23 09:26:46 -0500

    add default 404 fallback

--- core/Router.php
+++ core/Router.php
@@ -5,15 +5,27 @@ namespace App\Core;
 class Router
 {
   protected array $routes = [];
+  protected $fallbackFn;
 
   public function get(string $route, callable $fn)
   {
     $this->routes["GET"][$route] = $fn;
+    $this->fallbackFn = self::defaultFallback();
+  }
+
+  private static function defaultFallback()
+  {
+    http_response_code(404);
+  }
+
+  public function fallback(callable $fn)
+  {
+    $this->fallbackFn = $fn;
   }
 
   public function resolve(string $method, string $route)
   {
-    $responseFn = $this->routes[$method][$route] ?? null;
+    $responseFn = $this->routes[$method][$route] ?? $this->fallbackFn;
     return $responseFn;
   }
 }
